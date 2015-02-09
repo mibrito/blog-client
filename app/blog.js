1d3d9534fbc9ccd14b3dc939e633cda11cec386b
Michele Brito <mibrito@dcc.ufmg.br>
2015-02-09 17:24:23 -0200

    Start login interface

--- app/blog.js
+++ app/blog.js
@@ -4,6 +4,6 @@ Blog.Services = angular.module('blog.services',['ngResource']);
 Blog.Routes = angular.module('blog.routes',['ngRoute']);
 
 
-Blog.Dependencies = ['blog.routes','blog.controllers', 'blog.services'];
+Blog.Dependencies = ['openlayers-directive','blog.routes','blog.controllers', 'blog.services'];
 
 angular.module('blog',Blog.Dependencies);
